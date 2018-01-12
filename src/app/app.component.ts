46f6406d32b18732a05d4eed5854984071c706cd
Cl3r1k <Cl3r1k2@gmail.com>
2018-01-12 17:33:37 +0600

    Builded basic UI and prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Recipe {
+    id: number;
+    title: string;
+    ingredients: string;
+    active: boolean;
+
+    constructor(id: number, title: string, ingedients: string, active: boolean) {
+        this.id = id;
+        this.title = title;
+        this.ingredients = ingedients;
+        this.active = active;
+    }
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+    hiddenSocialIcons = true;
+    recipeList: Recipe[] = [
+        new Recipe(0, 'cake', 'potato, oil, apple', false),
+        new Recipe(1, 'cake with lemon', 'potato, oil, lemon', true),
+        new Recipe(2, 'fried chicken', 'chicken, oil, onion', false)];
 
     constructor() { }
 
@@ -18,6 +38,20 @@ export class AppComponent implements OnInit {
     }
 
     initApp() {
+        this.hiddenSocialIcons = false;
+    }
+
+    toggleSocialIconsState() {
+        this.hiddenSocialIcons = !this.hiddenSocialIcons;
+    }
+
+    addNewRecipe() {
         //
+    }
+
+    enlarge(recipeId: number) {
+        this.recipeList.forEach(item => {
+            item.active = item.id === recipeId && item.active === false ? true : false;
+        });
     }
 }
