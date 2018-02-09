d072632d9aa4bfc13cad9134491cd46345a00d5b
Cl3r1k <Cl3r1k2@gmail.com>
2018-02-09 17:02:13 +0600

    Added Material and CDK And many other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+// Library
+import { MatDialogModule, MatButtonModule, MatCardModule } from '@angular/material';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DialogDemoComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
