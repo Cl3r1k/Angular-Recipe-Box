d8b9d56bf7dfeecb6741e736b259e86989724696
Cl3r1k <Cl3r1k2@gmail.com>
2018-03-09 09:26:26 +0600

    Updated ssri to ssri-5.2.4 Updates in tests

--- src/app/my-dialog/my-dialog.component.spec.ts
+++ src/app/my-dialog/my-dialog.component.spec.ts
@@ -1,25 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+
 import { MyDialogComponent } from './my-dialog.component';
+import { MatFormFieldModule, MatDialogModule, MatDialogRef } from '@angular/material';
 
 describe('MyDialogComponent', () => {
-  let component: MyDialogComponent;
-  let fixture: ComponentFixture<MyDialogComponent>;
+    let component: MyDialogComponent;
+    let fixture: ComponentFixture<MyDialogComponent>;
+    const matDialogRefStub = {};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyDialogComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MyDialogComponent],
+            imports: [FormsModule, MatFormFieldModule, MatDialogModule],
+            providers: [
+                { provider: MatDialogRef, useValue: matDialogRefStub}
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MyDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
